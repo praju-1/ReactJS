c2610802cdfb34fff8728f78138a344f84a79d73
praju-1 <prajaktatayde26@gmail.com>
2024-01-12 12:57:47 +0530

    Updated

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+// import ArgumentinEvent from './EventComponent/ArgumentinEvent';
+// import Eventbinding from './EventComponent/Eventbinding';
+// import ArrayClassDestructor from './Components/ArrayClassDestructor';
+// import EventClass from './EventComponent/EventClass';
+// import EventClick from './EventComponent/EventClick';
+// import ArrayDestructor from './Components/ArrayDestructor';
+// import Counter from './Components/Counter';
+// import Message from './Components/Message';
+// import TypeProp from './Components/TypeProp';
+// import Welcome from './Components/Welcome';
+// import Greet from './Components/Greet';
+// import ParentComponent from './MethodsAsprops/ParentComponent'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is app.js files output</h1>
+      {/* <Greet name = "John"  skills = "MEAN Developer"> 
+      <p>This is child props</p>
+      </Greet> */}
+      {/* <Greet name = "Prince" skills = "MERN Developer"> 
+      <button>Click Me</button>
+      </Greet> */}
+      {/* <Greet name = "Victor" courseName = "Python Developer"/> */}
+
+      {/* <Welcome name = "John" city = "Pune" />
+      <Welcome name = "Prince" city = "Mumbai" />
+      <Welcome name = "Victor" city = "Banglore" /> */}
+
+      {/* <TypeProp myName = "John" myNumber = {123}  value = {[1, 2, 3,4]} anytype = {345} boolean = {true}/> */}
+      {/* <TypeProp myNumber = {123} /> */}
+      {/* <TypeProp value = '[1, 2, 3,4]' />
+      <TypeProp anytype = {345} />
+      <TypeProp boolean = 'this is' /> */}
+
+
+      {/* <Message /> */}
+      {/* <Counter /> */}
+      {/* <Welcome name = "Victor" courseName = "Python FullStack" /> */}
+
+      {/* <ArrayDestructor/> */}
+
+      {/* <ArrayClassDestructor/> */}
+
+      {/* <EventClick/> */}
+      {/* <EventClass/> */}
+
+
+      {/* <Eventbinding/> */}
+
+      {/* <ArgumentinEvent /> */}
+
+      {/* <ParentComponent /> */}
+
     </div>
+
   );
 }
 
